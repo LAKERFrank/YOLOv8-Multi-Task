2c2f2387ca8d103a74424941c2a40b69c37e4548
BartekTao <bartek_tao@cmoney.com.tw>
2024-10-27 21:22:51 +0800

    feat: save all image in val mode

--- ultralytics/tracknet/val.py
+++ ultralytics/tracknet/val.py
@@ -350,7 +350,7 @@ class TrackNetValidator(BaseValidator):
         mask_has_ball = mask_has_ball.view(self.num_groups*20*20).bool()
 
         ## save image
-        for frame_idx in [0, 4, 8]:
+        for frame_idx in range(10):
         
             each_probs = pred_probs.view(10, 20, 20)
             each_pos_x, each_pos_y = pred_pos.view(10, 20, 20, 2).split([1, 1], dim=3)
