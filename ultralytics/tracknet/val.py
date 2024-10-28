ccabc4b9212ca3c7c9b1d38c482d2d63b701f754
BartekTao <bartek_tao@cmoney.com.tw>
2024-10-28 19:55:06 +0800

    test: tolerance = 10, not save img

--- ultralytics/tracknet/val.py
+++ ultralytics/tracknet/val.py
@@ -387,7 +387,7 @@ class TrackNetValidator(BaseValidator):
 
             ball_count = mask_has_ball.sum()
             self.ball_count += ball_count   
-            tolerance = 5.0
+            tolerance = 10.0
             distance = torch.sqrt((pred_x - target_x) ** 2 + (pred_y - target_y) ** 2)
             
             box_color = 'red'
@@ -429,12 +429,12 @@ class TrackNetValidator(BaseValidator):
             now = datetime.now()
             # Format the datetime object as a string
             formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
-            display_predict_image(
-                    batch_img[frame_idx],  
-                    metrics, 
-                    'val_'+formatted_date+'_'+ str(int(batch_target[frame_idx][0])),
-                    box_color=box_color
-                    )  
+            # display_predict_image(
+            #         batch_img[frame_idx],  
+            #         metrics, 
+            #         'val_'+formatted_date+'_'+ str(int(batch_target[frame_idx][0])),
+            #         box_color=box_color
+            #         )  
             
 
         # 計算 conf 的 confusion matrix
