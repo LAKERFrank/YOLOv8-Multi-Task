99c1662891eadef2fe78d76a696a6afe08171958
BartekTao <bartek_tao@cmoney.com.tw>
2024-11-03 20:41:59 +0800

    switch

--- ultralytics/tracknet/val.py
+++ ultralytics/tracknet/val.py
@@ -455,13 +455,13 @@ class TrackNetValidator(BaseValidator):
             now = datetime.now()
             # Format the datetime object as a string
             formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
-            display_predict_image(
-                    batch_img[frame_idx],  
-                    metrics, 
-                    'val_'+formatted_date+'_'+ str(int(batch_target[frame_idx][0])),
-                    box_color=box_color,
-                    label=label
-                    )  
+            # display_predict_image(
+            #         batch_img[frame_idx],  
+            #         metrics, 
+            #         'val_'+formatted_date+'_'+ str(int(batch_target[frame_idx][0])),
+            #         box_color=box_color,
+            #         label=label
+            #         )  
             
 
         # 計算 conf 的 confusion matrix
