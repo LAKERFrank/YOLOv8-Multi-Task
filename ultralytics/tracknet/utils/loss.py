eecbdc55de44b96d709240ead2d26e0b40f5890b
BartekTao <bartek_tao@cmoney.com.tw>
2024-11-11 02:15:56 +0800

    test

--- ultralytics/tracknet/utils/loss.py
+++ ultralytics/tracknet/utils/loss.py
@@ -500,6 +500,7 @@ class FocalLossWithMask(nn.Module):
         """Calculates and updates confusion matrix for object detection/classification tasks."""
         loss = F.binary_cross_entropy_with_logits(pred, label, reduction='none')
         if (loss < 0).sum() > 0:
+            print(loss[loss < 0])
             print("bcs loss error loss has negative value")
         # p_t = torch.exp(-loss)
         # loss *= self.alpha * (1.000001 - p_t) ** self.gamma  # non-zero power for gradient stability
