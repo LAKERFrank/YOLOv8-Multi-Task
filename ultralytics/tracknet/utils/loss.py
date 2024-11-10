5fb37c57d5373cd006f8e7f070fd2516e47faead
BartekTao <bartek_tao@cmoney.com.tw>
2024-11-11 02:17:09 +0800

    test

--- ultralytics/tracknet/utils/loss.py
+++ ultralytics/tracknet/utils/loss.py
@@ -498,6 +498,9 @@ class FocalLossWithMask(nn.Module):
 
     def forward(self, pred, label, may_has_ball, mask_fast_ball, mask_hit_ball, gamma=2, alpha=0.75, negative_ratio=3.0):
         """Calculates and updates confusion matrix for object detection/classification tasks."""
+        if (label < 0).sum() > 0:
+            print(label[label < 0])
+            print("label has negative value")
         loss = F.binary_cross_entropy_with_logits(pred, label, reduction='none')
         if (loss < 0).sum() > 0:
             print(loss[loss < 0])
