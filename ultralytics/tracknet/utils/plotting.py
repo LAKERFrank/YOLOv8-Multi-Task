ca006848b8c817b681e76df7d9a579bab50f9ece
BartekTao <bartek_tao@cmoney.com.tw>
2024-12-02 20:04:33 +0800

    test

--- ultralytics/tracknet/utils/plotting.py
+++ ultralytics/tracknet/utils/plotting.py
@@ -141,6 +141,8 @@ def display_image_with_coordinates(img_tensor, target, pred, fileName, input_num
     plt.close()
 
 def display_predict_image(img_tensor, preds, fileName, input_number = None, box_color = 'blue', target = None, label = None, save_dir = Path('.'), stride = 32):
+    if isinstance(stride, torch.Tensor):
+        stride = stride.item()  # 將 tensor 轉換為純數值
     # Convert the image tensor to numpy array
     img_array = img_tensor.cpu().numpy()
 
