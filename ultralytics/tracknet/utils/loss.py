2cd2febfc7a06c7dc829bf525cdc96e81c68c52b
BartekTao <bartek_tao@cmoney.com.tw>
2024-12-08 17:48:07 +0800

    fix

--- ultralytics/tracknet/utils/loss.py
+++ ultralytics/tracknet/utils/loss.py
@@ -324,14 +324,14 @@ class TrackNetLoss:
                     t_x = (grid_x*stride+center*stride-target[2])/stride
                     t_y = (grid_y*stride+center*stride-target[3])/stride
                     if t_x >= 0:
-                        target_pos_distri[idx, target_idx, grid_y, grid_x, 0] = clamp(t_x, 0, self.reg_max - 0.01)
+                        target_pos_distri[idx, target_idx, grid_y, grid_x, 0] = clamp(t_x, 0, self.reg_max - 0.05)
                     else:
-                        target_pos_distri[idx, target_idx, grid_y, grid_x, 1] = clamp(-t_x, 0, self.reg_max - 0.01)
+                        target_pos_distri[idx, target_idx, grid_y, grid_x, 1] = clamp(-t_x, 0, self.reg_max - 0.05)
 
                     if t_y >= 0:
-                        target_pos_distri[idx, target_idx, grid_y, grid_x, 2] = clamp(t_y, 0, self.reg_max - 0.01)
+                        target_pos_distri[idx, target_idx, grid_y, grid_x, 2] = clamp(t_y, 0, self.reg_max - 0.05)
                     else:
-                        target_pos_distri[idx, target_idx, grid_y, grid_x, 3] = clamp(-t_y, 0, self.reg_max - 0.01)
+                        target_pos_distri[idx, target_idx, grid_y, grid_x, 3] = clamp(-t_y, 0, self.reg_max - 0.05)
 
                     ## cls
                     cls_targets[idx, target_idx, grid_y, grid_x, 0] = 1
@@ -343,16 +343,15 @@ class TrackNetLoss:
                         next_gty = batch_target[idx][target_idx+1][3]
                         next_t_x = (grid_x*stride+center*stride-next_gtx)/stride
                         next_t_y = (grid_y*stride+center*stride-next_gty)/stride
-                        print(next_t_x, next_t_y)
                         if next_t_x >= 0:
-                            target_pos_distri[idx, target_idx, grid_y, grid_x, 4] = clamp(next_t_x, 0, self.reg_max - 0.01)
+                            target_pos_distri[idx, target_idx, grid_y, grid_x, 4] = clamp(next_t_x, 0, self.reg_max - 0.05)
                         else:
-                            target_pos_distri[idx, target_idx, grid_y, grid_x, 5] = clamp(-next_t_x, 0, self.reg_max - 0.01)
+                            target_pos_distri[idx, target_idx, grid_y, grid_x, 5] = clamp(-next_t_x, 0, self.reg_max - 0.05)
 
                         if next_t_y >= 0:
-                            target_pos_distri[idx, target_idx, grid_y, grid_x, 6] = clamp(next_t_y, 0, self.reg_max - 0.01)
+                            target_pos_distri[idx, target_idx, grid_y, grid_x, 6] = clamp(next_t_y, 0, self.reg_max - 0.05)
                         else:
-                            target_pos_distri[idx, target_idx, grid_y, grid_x, 7] = clamp(-next_t_y, 0, self.reg_max - 0.01)
+                            target_pos_distri[idx, target_idx, grid_y, grid_x, 7] = clamp(-next_t_y, 0, self.reg_max - 0.05)
 
         mask_may_has_ball = F.max_pool2d(mask_has_ball, kernel_size=3, stride=1, padding=1)
         target_scores_sum = max(cls_targets.sum(), 1)
@@ -938,7 +937,7 @@ class XYLoss(nn.Module):
 
         # DFL loss
         if self.use_dfl:
-            assert target_pos_distri.min() >= 0 and target_pos_distri.max() <= self.reg_max, \
+            assert target_pos_distri.min() >= 0 and target_pos_distri.max() <= self.reg_max+1, \
                 f"Target out of range: min={target_pos_distri.min()}, max={target_pos_distri.max()}"
 
             loss_dfl = self._df_loss(pred_dist[fg_mask].view(-1, self.reg_max + 1), target_pos_distri[fg_mask]) * weight
