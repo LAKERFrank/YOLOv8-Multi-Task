7497d761f10bbc6a832d9abcd83c10a9bca6af55
BartekTao <bartek_tao@cmoney.com.tw>
2024-12-08 22:16:22 +0800

    test

--- ultralytics/tracknet/utils/loss.py
+++ ultralytics/tracknet/utils/loss.py
@@ -344,21 +344,21 @@ class TrackNetLoss:
                         next_grid_x, next_grid_y, _, _ = target_grid(next_gtx, next_gty, stride)
                         next_t_x = (grid_x*stride+center*stride-next_gtx)/stride
                         next_t_y = (grid_y*stride+center*stride-next_gty)/stride
-                        print(next_t_x, next_t_y)
                         if next_t_x >= 0:
-                            target_pos_distri[idx, target_idx, grid_y, grid_x, 4] = clamp(next_t_x, 0, self.reg_max - 0.1)
+                            target_pos_distri[idx, target_idx, grid_y, grid_x, 4] = clamp(next_t_x, 0, self.reg_max - 0.01)
                         else:
-                            target_pos_distri[idx, target_idx, grid_y, grid_x, 5] = clamp(-next_t_x, 0, self.reg_max - 0.1)
+                            target_pos_distri[idx, target_idx, grid_y, grid_x, 5] = clamp(-next_t_x, 0, self.reg_max - 0.01)
 
                         if next_t_y >= 0:
-                            target_pos_distri[idx, target_idx, grid_y, grid_x, 6] = clamp(next_t_y, 0, self.reg_max - 0.1)
+                            target_pos_distri[idx, target_idx, grid_y, grid_x, 6] = clamp(next_t_y, 0, self.reg_max - 0.01)
                         else:
-                            target_pos_distri[idx, target_idx, grid_y, grid_x, 7] = clamp(-next_t_y, 0, self.reg_max - 0.1)
+                            target_pos_distri[idx, target_idx, grid_y, grid_x, 7] = clamp(-next_t_y, 0, self.reg_max - 0.01)
 
         mask_may_has_ball = F.max_pool2d(mask_has_ball, kernel_size=3, stride=1, padding=1)
         target_scores_sum = max(cls_targets.sum(), 1)
 
         target_pos_distri = target_pos_distri.view(b, self.num_groups*cell_num*cell_num, self.feat_no)
+        print(target_pos_distri.shape)
         cls_targets = cls_targets.view(b, self.num_groups*cell_num*cell_num, 1)
         mask_has_ball = mask_has_ball.view(b, self.num_groups*cell_num*cell_num).bool()
         mask_may_has_ball = mask_may_has_ball.view(b, self.num_groups*cell_num*cell_num, 1).bool()
