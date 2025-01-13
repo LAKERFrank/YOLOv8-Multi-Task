92d787e4dd91d930e2a1517aa525c3fc9bdb680e
BartekTao <bartek_tao@cmoney.com.tw>
2025-01-13 09:01:14 +0800

    test

--- ultralytics/tracknet/configurable_dataset.py
+++ ultralytics/tracknet/configurable_dataset.py
@@ -21,10 +21,10 @@ class TrackNetConfigurableDataset(Dataset):
         self.samples = []
         self.prefix = prefix
         self.path_counts = { 
-            "profession_match_1" : 20, 
-            "profession_match_2" : 20, 
-            "profession_match_3" : 20,
-            "profession_match_4" : 20 }
+            "profession_match_1" : 150, 
+            "profession_match_2" : 150, 
+            "profession_match_3" : 150,
+            "profession_match_4" : 150 }
 
         self.idx = set()
 
