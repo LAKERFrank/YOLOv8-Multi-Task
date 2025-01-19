266ff43df8a18042fe9afe0430e4c72e3a6a88c9
BartekTao <bartek_tao@cmoney.com.tw>
2025-01-19 22:18:16 +0800

    test

--- ultralytics/tracknet/configurable_dataset.py
+++ ultralytics/tracknet/configurable_dataset.py
@@ -24,10 +24,10 @@ class TrackNetConfigurableDataset(Dataset):
             "match_1" : 100, 
             "match_2" : 20, 
             "blion_tracknet_partial": 473,
-            "profession_match_1" : 500, 
-            "profession_match_2" : 500, 
-            "profession_match_3" : 500,
-            "profession_match_4" : 500 }
+            "profession_match_1" : 100, 
+            "profession_match_2" : 100, 
+            "profession_match_3" : 100,
+            "profession_match_4" : 100 }
 
         self.idx = set()
 
