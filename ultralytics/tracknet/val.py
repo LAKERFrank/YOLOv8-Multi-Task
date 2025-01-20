3f1c06f552fb83af45d03dd991ea675ef388c455
BartekTao <bartek_tao@cmoney.com.tw>
2025-01-20 20:19:57 +0800

    test

--- ultralytics/tracknet/val.py
+++ ultralytics/tracknet/val.py
@@ -651,16 +651,16 @@ class TrackNetValidator(BaseValidator):
                 else:
                     target_xy = (batch_target[frame_idx][2], batch_target[frame_idx][3], batch_target[frame_idx][2], batch_target[frame_idx][3])
 
-                display_predict_image(
-                        batch_img[frame_idx],  
-                        metrics, 
-                        'val_'+formatted_date+'_'+ str(int(batch_target[frame_idx][0])),
-                        box_color=box_color,
-                        label=label,
-                        save_dir=self.metrics.save_dir,
-                        stride = self.stride,
-                        target=target_xy
-                        ) 
+                # display_predict_image(
+                #         batch_img[frame_idx],  
+                #         metrics, 
+                #         'val_'+formatted_date+'_'+ str(int(batch_target[frame_idx][0])),
+                #         box_color=box_color,
+                #         label=label,
+                #         save_dir=self.metrics.save_dir,
+                #         stride = self.stride,
+                #         target=target_xy
+                #         ) 
             
                 if box_color == 'blue':
                     display_predict_image(
@@ -689,18 +689,18 @@ class TrackNetValidator(BaseValidator):
                         next=False
                         ) 
 
-                display_predict_image(
-                            batch_img[frame_idx],  
-                            list(self.frame_10_metrics), 
-                            'val_'+formatted_date+'_'+ str(int(batch_target[frame_idx][0])),
-                            box_color=box_color,
-                            label=label,
-                            save_dir=self.metrics.save_dir,
-                            stride = self.stride,
-                            path='predict_val_10_frame_img',
-                            next=False,
-                            only_ball=True
-                            )
+                # display_predict_image(
+                #             batch_img[frame_idx],  
+                #             list(self.frame_10_metrics), 
+                #             'val_'+formatted_date+'_'+ str(int(batch_target[frame_idx][0])),
+                #             box_color=box_color,
+                #             label=label,
+                #             save_dir=self.metrics.save_dir,
+                #             stride = self.stride,
+                #             path='predict_val_10_frame_img',
+                #             next=False,
+                #             only_ball=True
+                #             )
 
                 # display_predict_image(
                 #             batch_img[frame_idx],  
