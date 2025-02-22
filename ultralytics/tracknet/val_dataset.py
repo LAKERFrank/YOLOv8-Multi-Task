961001983aa5cd09fa87d823c8e6efe69f61406d
BartekTao <bartek_tao@cmoney.com.tw>
2025-02-22 18:09:51 +0800

    always recreate cache

--- ultralytics/tracknet/val_dataset.py
+++ ultralytics/tracknet/val_dataset.py
@@ -125,8 +125,8 @@ class TrackNetValDataset(Dataset):
         return f
 
     def img_cache(self, match_name, video_name, img_files, npy_path):
-        if os.path.isfile(npy_path):
-            return
+        # if os.path.isfile(npy_path):
+        #     return
 
         # generate cache
         # 讀取影像並轉換為 `float32`，確保計算精度
