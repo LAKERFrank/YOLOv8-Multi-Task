09c53e089e89365ef16b2dd86075052c6bde97d6
BartekTao <bartek_tao@cmoney.com.tw>
2025-03-02 07:59:34 +0800

    open conf loss online_hard_example_mining

--- ultralytics/tracknet/utils/loss.py
+++ ultralytics/tracknet/utils/loss.py
@@ -862,8 +862,8 @@ class FocalLossWithMask(nn.Module):
 
         neg_mask = torch.zeros_like(labels, dtype=torch.bool)
         for i in range(loss.size(0)):  
-            # num_neg_samples = int(num_neg[i].item()) if int(num_neg[i].item()) != 0 else int(negative_ratio)
-            num_neg_samples = 640
+            num_neg_samples = int(num_neg[i].item()) if int(num_neg[i].item()) != 0 else int(negative_ratio)
+            # num_neg_samples = 640
             # num_neg_samples = int(num_neg[i].item())
             neg_mask[i, indices[i, :num_neg_samples]] = True 
 
@@ -898,13 +898,13 @@ class FocalLossWithMask(nn.Module):
         FP_mask = (pred_prob >= 0.5) & (label == 0)  # False Positive
 
         # Combine the masks (we only care about TP, FN, FP)
-        # relevant_mask = self.online_hard_example_mining(loss, label, negative_ratio)
+        relevant_mask = self.online_hard_example_mining(loss, label, negative_ratio)
 
         pos_no = label.sum() if label.sum() != 0 else 1
 
         w = (alpha/(1-alpha))
 
-        # loss = loss * relevant_mask.float()
+        loss = loss * relevant_mask.float()
 
         # loss[FN_mask] *= negative_ratio*20*w
         # loss[FP_mask & ~may_has_ball] *= negative_ratio*15*w
@@ -917,8 +917,9 @@ class FocalLossWithMask(nn.Module):
 
         # Apply the mask to the loss
         loss_sum = (loss).sum()
-        count = (pred_prob >= 0.5) | (label == 1)
-        loss = loss_sum / max(count.sum(), 1) if loss_sum != 0 else 0
+        loss = loss_sum / max(relevant_mask.float().sum(), 1) if loss_sum != 0 else 0
+        # count = (pred_prob >= 0.5) | (label == 1)
+        # loss = loss_sum / max(count.sum(), 1) if loss_sum != 0 else 0
 
         return loss
 
