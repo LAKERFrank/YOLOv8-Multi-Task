9d87120f3108b77bee8f8e654264b94e9a1617f5
BartekTao <bartek_tao@cmoney.com.tw>
2025-03-19 02:44:51 +0800

    test

--- ultralytics/tracknet/utils/loss.py
+++ ultralytics/tracknet/utils/loss.py
@@ -296,7 +296,7 @@ class TrackNetLoss:
                             if next_t_x >= self.reg_max - 1 or next_t_y >= self.reg_max - 1:
                                 target_pos_distri[idx, target_idx, abs_idx, 4:] = pred_pos[idx, target_idx * abs_idx, 4:]
                                 exceed_count += 1
-                                # print(f"warning 超過可預測範圍: next_t_x: {next_t_x}, next_t_y: {next_t_y}")
+                                print(f"warning 超過可預測範圍: next_t_x: {next_t_x}, next_t_y: {next_t_y}")
                                 continue
 
                             mask_has_next_ball[idx, target_idx, abs_idx] = 1
@@ -316,7 +316,7 @@ class TrackNetLoss:
 
             offset += cell_num * cell_num  # Move to next scale
 
-        print (f"exceed_count: {exceed_count}")
+        # print (f"exceed_count: {exceed_count}")
         # Flatten tensors for loss computation
         target_pos_distri = target_pos_distri.view(b, self.num_groups * total_cells, feature_dim)
         cls_targets = cls_targets.view(b, self.num_groups * total_cells, self.nc)
