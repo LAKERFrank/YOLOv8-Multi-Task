7727b5f528f353af8589a4b01ac77fa1d1241741
BartekTao <bartek_tao@cmoney.com.tw>
2025-03-21 23:35:40 +0800

    refactor: Split predicted position distributions for improved handling of target positions and next ball predictions

--- ultralytics/tracknet/utils/loss.py
+++ ultralytics/tracknet/utils/loss.py
@@ -236,17 +236,22 @@ class TrackNetLoss:
             pred_distri = pred_distri.permute(0, 2, 1).contiguous()
 
             b, a, c = pred_distri.shape  # batch, anchors, channels
-            pred_pos_distri = pred_distri
-            pred_pos = pred_pos_distri.view(b, a, self.feat_no, c // self.feat_no).softmax(3).matmul(
-                self.proj.type(pred_distri.dtype))
+            pred_pos_distri, n_pred_pos_distri = pred_distri.split([c//2, c//2], 2)
+            pred_pos = pred_pos_distri.view(b, a, self.feat_no//2, self.reg_max).softmax(3).matmul(
+                self.proj.type(pred_pos_distri.dtype))
+            n_pred_pos = n_pred_pos_distri.view(b, a, self.feat_no//2, self.reg_max).softmax(3).matmul(
+                self.proj.type(n_pred_pos_distri.dtype))
 
             batch_target = batch['target'].to(self.device)
 
             cell_num = int(640/self.stride[i])
-            target_pos_distri = torch.zeros(b, self.num_groups, cell_num, cell_num, self.feat_no, device=self.device)
+            target_pos_distri = torch.zeros(b, self.num_groups, cell_num, cell_num, self.feat_no//2, device=self.device)
             mask_has_ball = torch.zeros(b, self.num_groups, cell_num, cell_num, device=self.device)
             cls_targets = torch.zeros(b, self.num_groups, cell_num, cell_num, 1, device=self.device)
+
+            n_target_pos_distri = torch.zeros(b, self.num_groups, cell_num, cell_num, self.feat_no//2, device=self.device)
             mask_has_next_ball = torch.zeros(b, self.num_groups, cell_num, cell_num, device=self.device)
+            n_cls_targets = torch.zeros(b, self.num_groups, cell_num, cell_num, 1, device=self.device)
 
             for idx, _ in enumerate(batch_target):
                 # pred = [330 * cell_num * cell_num]
@@ -281,42 +286,43 @@ class TrackNetLoss:
                         cls_targets[idx, target_idx, grid_y, grid_x, 0] = 1
 
                         if (target[4] != 0 or target[5] != 0) and target_idx < len(batch_target[idx]) - 1:
-                            mask_has_next_ball[idx, target_idx, grid_y, grid_x] = 1
-
                             next_gtx = target[4]
                             next_gty = target[5]
                             next_t_x = (grid_x*stride+center*stride-next_gtx)*8/stride
                             next_t_y = (grid_y*stride+center*stride-next_gty)*8/stride
+
                             if abs(next_t_x) >= self.reg_max - 1 or abs(next_t_y) >= self.reg_max - 1:
                                 print(f"warning 超過可預測範圍: stride: {stride} next_t_x: {next_t_x}, next_t_y: {next_t_y}")
-                                target_pos_distri[idx, target_idx, grid_y, grid_x, 4] = pred_pos_distri[idx, target_idx*grid_y*grid_x, 4]
-                                target_pos_distri[idx, target_idx, grid_y, grid_x, 5] = pred_pos_distri[idx, target_idx*grid_y*grid_x, 5]
-                                target_pos_distri[idx, target_idx, grid_y, grid_x, 6] = pred_pos_distri[idx, target_idx*grid_y*grid_x, 6]
-                                target_pos_distri[idx, target_idx, grid_y, grid_x, 7] = pred_pos_distri[idx, target_idx*grid_y*grid_x, 7]
                                 continue
+
+                            mask_has_next_ball[idx, target_idx, grid_y, grid_x] = 1
+                            n_cls_targets[idx, target_idx, grid_y, grid_x, 0] = 1
+
                             if next_t_x >= 0:
-                                target_pos_distri[idx, target_idx, grid_y, grid_x, 4] = clamp(next_t_x, 0, self.reg_max-1 - 0.01)
+                                n_target_pos_distri[idx, target_idx, grid_y, grid_x, 0] = clamp(next_t_x, 0, self.reg_max-1 - 0.01)
                             else:
-                                target_pos_distri[idx, target_idx, grid_y, grid_x, 5] = clamp(-next_t_x, 0, self.reg_max-1 - 0.01)
+                                n_target_pos_distri[idx, target_idx, grid_y, grid_x, 1] = clamp(-next_t_x, 0, self.reg_max-1 - 0.01)
 
                             if next_t_y >= 0:
-                                target_pos_distri[idx, target_idx, grid_y, grid_x, 6] = clamp(next_t_y, 0, self.reg_max-1 - 0.01)
+                                n_target_pos_distri[idx, target_idx, grid_y, grid_x, 2] = clamp(next_t_y, 0, self.reg_max-1 - 0.01)
                             else:
-                                target_pos_distri[idx, target_idx, grid_y, grid_x, 7] = clamp(-next_t_y, 0, self.reg_max-1 - 0.01)
-                        else:
-                            target_pos_distri[idx, target_idx, grid_y, grid_x, 4] = pred_pos_distri[idx, target_idx*grid_y*grid_x, 4]
-                            target_pos_distri[idx, target_idx, grid_y, grid_x, 5] = pred_pos_distri[idx, target_idx*grid_y*grid_x, 5]
-                            target_pos_distri[idx, target_idx, grid_y, grid_x, 6] = pred_pos_distri[idx, target_idx*grid_y*grid_x, 6]
-                            target_pos_distri[idx, target_idx, grid_y, grid_x, 7] = pred_pos_distri[idx, target_idx*grid_y*grid_x, 7]
+                                n_target_pos_distri[idx, target_idx, grid_y, grid_x, 3] = clamp(-next_t_y, 0, self.reg_max-1 - 0.01)
 
             target_scores_sum = max(cls_targets.sum(), 1)
+            n_target_scores_sum = max(n_cls_targets.sum(), 1)
 
-            target_pos_distri = target_pos_distri.view(b, self.num_groups*cell_num*cell_num, self.feat_no)
+            target_pos_distri = target_pos_distri.view(b, self.num_groups*cell_num*cell_num, self.feat_no//2)
             cls_targets = cls_targets.view(b, self.num_groups*cell_num*cell_num, 1)
             mask_has_ball = mask_has_ball.view(b, self.num_groups*cell_num*cell_num).bool()
+
+            n_target_pos_distri = n_target_pos_distri.view(b, self.num_groups*cell_num*cell_num, self.feat_no//2)
+            n_cls_targets = n_cls_targets.view(b, self.num_groups*cell_num*cell_num, 1)
+            mask_has_next_ball = mask_has_next_ball.view(b, self.num_groups*cell_num*cell_num).bool()
             
             _, xy_loss = self.xy_loss(pred_pos_distri, pred_pos, target_pos_distri, cls_targets, target_scores_sum, mask_has_ball)
+            _, nxny_loss = self.xy_loss(n_pred_pos_distri, n_pred_pos, n_target_pos_distri, n_cls_targets, n_target_scores_sum, mask_has_next_ball)
             loss[0] += xy_loss
+            loss[0] += nxny_loss
             cls_targets = cls_targets.to(pred_scores.dtype)
 
             self.confusion_class.confusion_matrix(pred_scores.sigmoid(), cls_targets)
