ace40de7277cb1dfd14278c0dd91c77d5c5fbecc
BartekTao <bartek_tao@cmoney.com.tw>
2025-04-04 11:08:09 +0800

    fix: 增加額外判斷，下顆球存在，才會計算 nx ny loss（因為有前處理，移除原先有球的 label)

--- ultralytics/tracknet/utils/loss.py
+++ ultralytics/tracknet/utils/loss.py
@@ -285,7 +285,7 @@ class TrackNetLoss:
                         ## cls
                         cls_targets[idx, target_idx, grid_y, grid_x, 0] = 1
 
-                        if (target[4] != 0 or target[5] != 0) and target_idx < len(batch_target[idx]) - 1:
+                        if (target[4] != 0 or target[5] != 0) and target_idx < len(batch_target[idx]) - 1 and batch_target[idx][target_idx + 1][1] == 1:
                             next_gtx = target[4]
                             next_gty = target[5]
                             next_t_x = (grid_x*stride+center*stride-next_gtx)*8/stride
