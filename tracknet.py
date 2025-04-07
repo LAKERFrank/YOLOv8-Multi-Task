7535665a21173ff3f32bc224078164592625ddf0
BartekTao <bartek_tao@cmoney.com.tw>
2025-04-07 08:13:19 +0800

    feat: Add training mode 'train_v3' with model freezing capability

--- tracknet.py
+++ tracknet.py
@@ -748,6 +748,10 @@ def main(arg):
     elif arg.mode == 'train_v2':
         model = TrackNet(overrides)
         model.train()
+    elif arg.mode == 'train_v3':
+        model = TrackNet(overrides)
+        model.freeze(8)
+        model.train()
     elif arg.mode == 'val_v2':
         model = TrackNet(overrides)
         model.val()
