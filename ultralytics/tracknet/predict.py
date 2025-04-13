98d0f91b039fb57a3aace1d75fc21898513c8a67
BartekTao <bartek_tao@cmoney.com.tw>
2025-04-14 00:16:28 +0800

    refactor TrackNetPredictor: fix tensor contiguity check for image conversion

--- ultralytics/tracknet/predict.py
+++ ultralytics/tracknet/predict.py
@@ -84,8 +84,8 @@ class TrackNetPredictor(BasePredictor):
             t2 = time.perf_counter()
             im = torch.from_numpy(im)    # 直接轉換, shape = (H, W, C)
             im = im.permute(2, 0, 1)  # (HWC -> CHW)
-            if not tensor_im.is_contiguous():
-                tensor_im = tensor_im.contiguous()
+            if not im.is_contiguous():
+                im = im.contiguous()
                 print("Tensor is not contiguous, converting to contiguous tensor.")
             t3 = time.perf_counter()
             timings["numpy_to_tensor"] = (t3 - t2) * 1000
