8b7e9c12ba2234a129084a2de8249c3b08a7fb37
BartekTao <bartek_tao@cmoney.com.tw>
2025-04-28 01:37:38 +0800

    adjust profiler schedule parameters for optimized performance monitoring

--- ultralytics/yolo/engine/predictor.py
+++ ultralytics/yolo/engine/predictor.py
@@ -360,7 +360,7 @@ class BasePredictor:
         # ✅ 在這裡開一個 global profiler
         with torch.profiler.profile(
             activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
-            schedule=torch.profiler.schedule(wait=1, warmup=1, active=20, repeat=1),
+            schedule=torch.profiler.schedule(wait=1, warmup=1, active=10, repeat=1),
             on_trace_ready=torch.profiler.tensorboard_trace_handler(profiler_output_dir),
             record_shapes=True,
             profile_memory=True,
