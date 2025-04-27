a82e84ce72d8fab05d61870db0faf140f57eb929
BartekTao <bartek_tao@cmoney.com.tw>
2025-04-27 22:54:57 +0800

    test

--- ultralytics/yolo/engine/predictor.py
+++ ultralytics/yolo/engine/predictor.py
@@ -335,9 +335,9 @@ class BasePredictor:
             pin_memory=True,
             prefetch_factor=8,
         )
-        preprocess_num_streams = 32
-        inference_num_streams = 32
-        postprocess_num_streams = 32
+        preprocess_num_streams = 1
+        inference_num_streams = 1
+        postprocess_num_streams = 1
 
         preprocess_streams = [torch.cuda.Stream() for _ in range(preprocess_num_streams)]
         inference_streams = [torch.cuda.Stream() for _ in range(inference_num_streams)]
