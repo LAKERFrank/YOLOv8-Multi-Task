8e0d0a1788b9f6f03a0f9c2a592ae8a12049a044
BartekTao <bartek_tao@cmoney.com.tw>
2025-04-29 11:51:28 +0800

    fix: enable logging for batch completion in predictor

--- ultralytics/yolo/engine/predictor.py
+++ ultralytics/yolo/engine/predictor.py
@@ -839,7 +839,6 @@ class BasePredictor:
             except Empty:
                 pass
             
-            LOGGER.info("start pending loop")
             next_pending = []
             for p in pending:
                 if p["event"].query():
@@ -863,9 +862,9 @@ class BasePredictor:
                     post_total += post_e
                     total_images += n
 
-                    # LOGGER.info(f"[COMPLETE] Batch {p['batch_idx']} on Stream-{p['stream_idx']} "
-                    #             f"Pre: {pre_e:.2f}ms, Infer: {infer_e:.2f}ms, Post: {post_e:.2f}ms, "
-                    #             f"Finished at {complete_time:.6f}s")
+                    LOGGER.info(f"[COMPLETE] Batch {p['batch_idx']} on Stream-{p['stream_idx']} "
+                                f"Pre: {pre_e:.2f}ms, Infer: {infer_e:.2f}ms, Post: {post_e:.2f}ms, "
+                                f"Finished at {complete_time:.6f}s")
 
                     for j in range(n):
                         p["results"][j].speed = {
