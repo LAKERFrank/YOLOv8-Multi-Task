fix: correct median calculation dimension in TrackNetPredictor

ultralytics/tracknet/predict.py
@@ -99,8 +99,7 @@ class TrackNetPredictor(BasePredictor):
             im = im.permute(2, 0, 1).contiguous()  # (HWC -> CHW)
 
         im = im.to(self.device, dtype=torch.float32, non_blocking=True)
-
-        median = im.median(dim=0).values  # shape: (H, W)
+        median = im.median(dim=1).values  # shape: (H, W)
         im.sub_(median).clamp_(0, 255).div_(255.0)
 
         # im = im.unsqueeze(0)