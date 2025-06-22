6f70e20710744aa18d06adc7d97ab42eb3b9ae87
Frank <54981806+LAKERFrank@users.noreply.github.com>
2025-06-22 22:40:47 +0800

    Use reshape for single-channel images

--- ultralytics/multitask/val_dataset.py
+++ ultralytics/multitask/val_dataset.py
@@ -298,7 +298,7 @@ class TrackNetValDataset(Dataset):
         img = self.pad_to_square(img, pad_value)
         #self.display_image(img)
         img = cv2.resize(img, dsize=(640, 640), interpolation=cv2.INTER_CUBIC)
-        img.resize((1, 640, 640))
+        img = img.reshape(1, 640, 640)
         return img
 
     def open_image(self, path):
@@ -500,7 +500,7 @@ class MultiTaskValDataset(Dataset):
         img = self.open_image(path)
         img = self.pad_to_square(img, pad_value)
         img = cv2.resize(img, dsize=(640, 640), interpolation=cv2.INTER_CUBIC)
-        img.resize((1, 640, 640))
+        img = img.reshape(1, 640, 640)
         return img
 
     def open_image(self, path):
--- ultralytics/tracknet/test_dataset.py
+++ ultralytics/tracknet/test_dataset.py
@@ -212,7 +212,7 @@ class TrackNetTestDataset(Dataset):
     def __preprocess_img(self, path, pad_value=0):
         img = self.open_image(path)
         img = self.pad_to_square(img, pad_value)
-        img.resize((1, 640, 640))
+        img = img.reshape(1, 640, 640)
         return img
 
     def open_image(self, path):
