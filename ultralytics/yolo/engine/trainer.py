b5d407ace71a59b39c74283822d4dfbd150b681a
Frank <54981806+LAKERFrank@users.noreply.github.com>
2025-06-29 01:09:30 +0800

    fix: call scheduler with epoch index

--- ultralytics/yolo/engine/trainer.py
+++ ultralytics/yolo/engine/trainer.py
@@ -453,7 +453,7 @@ class BaseTrainer:
             self.lr = {f'lr/pg{ir}': x['lr'] for ir, x in enumerate(self.optimizer.param_groups)}  # for loggers
 
             if last_opt_step >= 0:
-                self.scheduler.step()
+                self.scheduler.step(epoch=epoch + 1)
             self.run_callbacks('on_train_epoch_end')
 
             if RANK in (-1, 0):
